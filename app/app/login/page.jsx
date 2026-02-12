000849c5342173220160304939bf58e53ebb8271
Jondoez2 <braiden.zeller@gmail.com>
2026-02-12 12:36:52 -0800

    Create page.jsx

---
+++ app/app/login/page.jsx
@@ -0,0 +1,40 @@
+"use client";
+import { useState } from "react";
+import { supabase } from "../../lib/supabase";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  async function handleLogin() {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) setMessage(error.message);
+    else setMessage("Logged in successfully.");
+  }
+
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Login</h1>
+
+      <input
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        placeholder="Password"
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={handleLogin}>Login</button>
+
+      <p>{message}</p>
+    </div>
+  );
+}
